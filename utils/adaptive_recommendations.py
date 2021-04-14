4becbc9f26c33a522f532139bdcf600e444d94f1
Thomas A Caswell <tcaswell@bnl.gov>
2021-04-14 16:09:27 -0400

    MNT: get demo working with on_stop reccomendor factory

--- utils/adaptive_recommendations.py
+++ utils/adaptive_recommendations.py
@@ -1,18 +1,9 @@
 from bluesky_adaptive.recommendations import NoRecommendation
 
-def nieve_agent(x, y):
+
+def naive_agent(x, y):
     """A simple naive agent"""
     return x + 1
-
-
-def environment_to_motor_position(env):
-    """Converts integer environment variable to motor position"""
-    pass
-
-
-def motor_position_to_environment(pos):
-    """Converts motor position to integer environment variable"""
-    pass
 
 
 class BadSeedRecommender:
@@ -21,32 +12,31 @@ class BadSeedRecommender:
     This should be a one-in-one-out recommender.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, num_samples, **kwargs):
         """Load the model, set up the necessary bits"""
         self.next_point = None
+        self.num_samples = num_samples
         self.agent = self.build_agent(*args, **kwargs)
 
     def build_agent(self, *args, **kwargs):
-        raise nieve_agent
-
-    def _preprocessing(self, x, y):
-        return environment_to_motor_position(x), y
-
-    def _postprocessing(self, x):
-        return motor_position_to_environment(x)
+        return naive_agent
 
     def tell(self, x, y):
         """Tell the recommnder about something new"""
-        x, y = self._preprocessing(x, y)
         self.next_point = self.agent(x, y)
+
+    def tell_many(self, xs, ys):
+        for x, y in zip(xs, ys):
+            self.tell(x, y)
 
     def ask(self, n, tell_pending=True):
         """Ask the recommender for a new command"""
         if n != 1:
             raise NotImplementedError
-        if self.next_point is None:
+        if self.next_point is None or self.next_point >= self.num_samples:
             raise NoRecommendation
-        return self._postprocessing(self.next_point)
+
+        return (self.next_point,)
 
 
 class RLRecommender(BadSeedRecommender):
@@ -54,30 +44,51 @@ class RLRecommender(BadSeedRecommender):
         super().__init__(*args, **kwargs)
 
     def build_agent(self, *args, **kwargs):
-        """Function to construct the RL agent from save points. Returns a callable f(x,y)."""
+        """Function to construct the RL agent from save points.
+
+        Returns
+        -------
+        agent : Callable[float, float] -> int
+
+           f(x, y) -> next_point
+
+        """
         raise NotImplementedError
 
 
-if __name__ == "__main__":
-    import bluesky_adaptive
-    import bluesky.plan_stubs as bps
-    from bluesky_adaptive.per_event import adaptive_plan, recommender_factory
+if True:
+    import numpy as np
+    from bluesky_adaptive.per_start import adaptive_plan
+    from bluesky_adaptive.on_stop import recommender_factory
+    from bluesky.callbacks.best_effort import BestEffortCallback
     from bluesky import RunEngine
     from ophyd.sim import hw
 
     hw = hw()
+    bec = BestEffortCallback()
 
-    def do_the_thing(det, det_key, key_of_goodness):
-        recommender = BadSeedRecommender()
+    def do_the_thing(det, key_of_goodness, sample_positions, max_shots=25):
+        sample_positions = np.array(sample_positions)
+
+        def motor_to_sample_indx(pos):
+            pos = pos.compute().data
+            return np.argmin(np.abs(sample_positions - pos))
+
+        def sample_indx_to_motor(indx):
+            return sample_positions[int(indx)]
+
+        recommender = BadSeedRecommender(num_samples=len(sample_positions))
         cb, queue = recommender_factory(
-            recommender,
-            independent_keys=["motor"],
-            dependent_keys=[det_key, key_of_goodness],
-            max_count=90,
+            adaptive_obj=recommender,
+            independent_keys=[lambda motor: motor_to_sample_indx(motor)],
+            dependent_keys=[key_of_goodness],
+            target_keys=["motor"],
+            target_transforms={"motor": sample_indx_to_motor},
+            max_count=max_shots,
         )
         yield from adaptive_plan(
             [det], {hw.motor: 1}, to_recommender=cb, from_recommender=queue
         )
 
-        RE = RunEngine()
-        RE(do_the_thing(hw.img, "img"), print)
+    RE = RunEngine()
+    RE(do_the_thing(hw.det, "int(motor % 2)", [0.5, 1, 1.25, 2, 3]), bec)
